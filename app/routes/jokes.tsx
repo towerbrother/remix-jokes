42c2e0957ecdc6b4ee37389a15917116d0c2e23c
GTO <giorgio.torre@stellenanzeigen.de>
2022-03-17 08:06:43 +0100

    setup login/logout UI

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -2,6 +2,7 @@ import type { LinksFunction, LoaderFunction } from "remix";
 import { json, Link, Outlet, useLoaderData } from "remix";
 
 import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
 import stylesUrl from "~/styles/jokes.css";
 
 export const links: LinksFunction = () => {
@@ -9,17 +10,23 @@ export const links: LinksFunction = () => {
 };
 
 type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
   jokeListItems: Array<{ id: string; name: string }>;
 };
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    select: { id: true, name: true },
+  });
+  const user = await getUser(request);
+
   const data: LoaderData = {
-    jokeListItems: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: "desc" },
-    }),
+    jokeListItems,
+    user,
   };
+
   return json(data);
 };
 
@@ -35,6 +42,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login/">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
