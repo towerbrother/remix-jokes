8b4c2dcf6fdd0b50039f58cbd2d564011db75786
GTO <giorgio.torre@stellenanzeigen.de>
2022-03-19 10:48:18 +0100

    added error boundary in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -53,3 +53,14 @@ export default function App() {
     </Document>
   );
 }
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title="Uh-oh!">
+      <div className="error-container">
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
+  );
+}
